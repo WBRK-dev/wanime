8ea552af1a53c5ef905e341d087f2b8d4c287c37
WBRK-dev <koertjrweberp@gmail.com>
2024-03-14 10:29:31 +0100

    redesign watchlist route

--- resources/views/watchlist.blade.php
+++ resources/views/watchlist.blade.php
@@ -6,17 +6,15 @@
     
     <div class="p-2">
         
-        <div class="d-flex justify-content-center">
-            <div class="btn-group flex-wrap rounded overflow-hidden">
-                <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=completed" class="btn btn-primary rounded-0 {{ request()->input("app") == "completed" ? "active" : "" }}">Completed</a>
-                <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=planning" class="btn btn-primary rounded-0 {{ request()->input("app") == "planning" ? "active" : "" }}">Planning</a>
-                <a href="{{ $_ENV["APP_URL"] }}/watchlist" class="btn btn-primary rounded-0 {{ request()->input("app") == "" ? "active" : "" }}" aria-current="page">Watching</a>
-                <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=paused" class="btn btn-primary rounded-0 {{ request()->input("app") == "paused" ? "active" : "" }}" aria-current="page">Paused</a>
-                <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=dropped" class="btn btn-primary rounded-0 {{ request()->input("app") == "dropped" ? "active" : "" }}" aria-current="page">Dropped</a>
-            </div>            
+        <div class="d-flex flex-wrap gap-2">
+            <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=completed" class="btn btn-{{ request()->input("app") == "completed" ? "primary" : "secondary" }}">Completed</a>
+            <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=planning" class="btn btn-{{ request()->input("app") == "planning" ? "primary" : "secondary" }}">Planning</a>
+            <a href="{{ $_ENV["APP_URL"] }}/watchlist" class="btn btn-{{ request()->input("app") == "" ? "primary" : "secondary" }}" aria-current="page">Watching</a>
+            <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=paused" class="btn btn-{{ request()->input("app") == "paused" ? "primary" : "secondary" }}" aria-current="page">Paused</a>
+            <a href="{{ $_ENV["APP_URL"] }}/watchlist?app=dropped" class="btn btn-{{ request()->input("app") == "dropped" ? "primary" : "secondary" }}" aria-current="page">Dropped</a>
         </div>
 
-        <div class="d-grid column-gap-2 mt-2 overflow-hidden" style="grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));">
+        <div class="d-grid column-gap-2 overflow-hidden" style="grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));">
             @foreach ($anime as $animeItem)
                 @include("modules.animecard", [
                     "id" => $animeItem["animeId"],
