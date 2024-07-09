314c80c1b7cdb45d285eb470bb131676096c59b0
WBRK-dev <koertjrweberp@gmail.com>
2024-07-09 21:08:43 +0200

    chore: added original models and migrations back into branch

---
+++ database/migrations/2024_01_06_124644_anime.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anime', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('animeId');
+            $table->string('image');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->unique("animeId");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anime');
+    }
+};
